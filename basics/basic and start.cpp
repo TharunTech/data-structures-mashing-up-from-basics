6eb1ddb8169475ffbf6099f35141b9382ac13e8f
Tharun <tharuntech9840@gmail.com>
2022-05-25 19:04:59 +0530

    s4

---
+++ basics/basic and start.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+
+// ARRAY
+int main()
+{
+    int A[10]={2,3,4,5,4,};
+    
+    cout<<sizeof(A)<<endl;
+    cout<<A[1]<<endl;
+    printf("%d\n",A[2]);
+
+    return 0;
+}
